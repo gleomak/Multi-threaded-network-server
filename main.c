89ea5cd87f49af42c1ab9e892a263541b8a66cba
gleomak <sdi1900105@di.uoa.gr>
2023-06-08 05:05:09 +0300

    Done?

--- main.c
+++ main.c
@@ -10,6 +10,27 @@
 #include <string.h>
 #include <unistd.h>
 
+typedef struct {
+    char *voterName;
+    char *vote;
+    struct VoteTuple *next;
+} VoteTuple;
+
+typedef struct {
+    int bufferSize;
+    int counter;
+    int start;
+    int end;
+    int *bufferArray;
+    VoteTuple *voterList;
+} GlobalData;
+
+int whileLoopBoolean = 0;
+pthread_mutex_t mtx;
+pthread_cond_t condNotFull;
+pthread_cond_t condNotEmpty;
+GlobalData globalData;
+
 #define lockMutex(mtx) \
     do { \
         int lockErr; \
@@ -29,35 +50,82 @@
     } while (0)
 
 
-int whileLoopBoolean = 0;
-
-typedef struct {
-    pthread_mutex_t mtx;
-    pthread_cond_t condNotFull;
-    pthread_cond_t condNotEmpty;
-    int bufferSize;
-    int counter;
-    int start;
-    int end;
-    int *bufferArray;
-
-} GlobalData;
-
-void * workerThread(void *args) {
-    GlobalData * globalData = (GlobalData *) args;
-    lockMutex(globalData->mtx);
-    while(globalData->counter == 0){
-        pthread_cond_wait(&globalData->condNotEmpty, &globalData->mtx);
+void trimStrings(char *string) {
+    uint len = strlen(string);
+    for (int i = 0; i < len; i++) {
+        if (string[i] == '\n' || string[i] == '\r') {
+            string[i] = '\0';
+            break; // Assuming there is only one occurrence of newline/carriage return
+        }
     }
-    char* sendNameMessage= "SEND NAME PLEASE";
-    write(globalData->bufferArray[globalData->start], sendNameMessage, strlen(sendNameMessage));
-    char buffer[100];
-    read(globalData->bufferArray[globalData->start], buffer, 100);
-    printf("Name is %s\n",buffer);
-    globalData->start = (globalData->start + 1) % globalData->bufferSize;
-    globalData->counter --;
-    unlockMutex(globalData->mtx);
-    pthread_cond_signal(&globalData->condNotFull);
+}
+
+void addVote(VoteTuple **vt, char *name, char *vote){
+    VoteTuple * newNode = (VoteTuple *)malloc(sizeof(VoteTuple));
+    if (newNode == NULL) {
+        perror("Failed to allocate memory for vote");
+        exit(1);
+    }
+
+    strcpy(newNode->voterName, name);
+    strcpy(newNode->vote, vote);
+    newNode->next = NULL;
+
+    if (*vt == NULL) {
+        *vt = newNode;
+    } else {
+        VoteTuple * current = *vt;
+        while (current->next != NULL) {
+            current = (VoteTuple *) current->next;
+        }
+        current->next = (struct VoteTuple *) newNode;
+    }
+}
+
+int findName(VoteTuple *vt, const char *name){
+    VoteTuple *current = vt;
+    while(current != NULL){
+        if(strcmp(current->voterName, name) == 0)
+            return 1;
+        current = (VoteTuple *) current->next;
+    }
+    return 0;
+}
+
+void initializeGlobalData(GlobalData *gd, int bufferSize) {
+    gd->bufferSize = bufferSize;
+    gd->end = -1;
+    gd->start = 0;
+    gd->counter = 0;
+    gd->bufferArray = malloc(bufferSize * sizeof(int));
+    gd->voterList = NULL;
+}
+
+void *workerThread(void *args) {
+    GlobalData *data = (GlobalData *) args;
+    lockMutex(mtx);
+    while (data->counter == 0) {
+        pthread_cond_wait(&condNotEmpty, &mtx);
+    }
+    char sendNameMessage[] = "SEND NAME PLEASE\n";
+    write(data->bufferArray[data->start], sendNameMessage, strlen(sendNameMessage));
+    char bufferName[100];
+    read(data->bufferArray[data->start], buffer, 100);
+//    printf("Name is %s\n", buffer);
+    char sendVoteMessage[] = "SEND VOTE PLEASE\n";
+    write(data->bufferArray[data->start], sendVoteMessage, strlen(sendVoteMessage));
+    char buffer2[100];
+    read(data->bufferArray[data->start], buffer2, 100);
+    printf("Name is %s, and Vote is %s\n", bufferName, buffer2);
+    trimStrings(bufferName);
+    if(!findName(globalData.voterList, bufferName)){
+        addVote()
+    }
+
+    data->start = (data->start + 1) % data->bufferSize;
+    data->counter--;
+    unlockMutex(mtx);
+    pthread_cond_signal(&condNotFull);
 //    return (void *) 1;
     return NULL;
 }
@@ -77,7 +145,7 @@ int main(int argc, char **argv) {
     int sock, newSock;
     struct sockaddr_in server, client;
     socklen_t clientlen = sizeof(client);
-    struct sockaddr *clientptr=(struct sockaddr *)&client;
+    struct sockaddr *clientptr = (struct sockaddr *) &client;
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
         exit(EXIT_FAILURE);
     server.sin_family = AF_INET;       /* Internet domain */
@@ -92,33 +160,28 @@ int main(int argc, char **argv) {
     printf("Listening for connections to port %d\n", portnum);
 
     pthread_t *workers = (pthread_t *) malloc(numOfWorkingThreads * sizeof(pthread_t));
-    GlobalData globalData;
-    pthread_mutex_init(&globalData.mtx, NULL);
-    pthread_cond_init(&globalData.condNotEmpty, NULL); /* Initialize condition variable */
-    pthread_cond_init(&globalData.condNotFull, NULL); /* Initialize condition variable */
-    globalData.bufferSize = bufferSize;
-    globalData.end = -1;
-    globalData.start = 0;
-    globalData.counter = 0;
-    globalData.bufferArray = malloc(bufferSize * sizeof(int));
+    initializeGlobalData(&globalData, bufferSize);
 
+    pthread_mutex_init(&mtx, 0);
+    pthread_cond_init(&condNotFull, 0); /* Initialize condition variable */
+    pthread_cond_init(&condNotEmpty, 0); /* Initialize condition variable */
 
     for (int i = 0; i < numOfWorkingThreads; i++) {
         pthread_create(&workers[i], NULL, workerThread, &globalData);
     }
+
     while (!whileLoopBoolean) {
-        lockMutex(globalData.mtx);
-        while(globalData.counter == bufferSize){
-            pthread_cond_wait(&globalData.condNotFull, &globalData.mtx);
+        lockMutex(mtx);
+        while (globalData.counter == bufferSize) {
+            pthread_cond_wait(&condNotFull, &mtx);
         }
-        if ((newSock = accept(sock, clientptr, &clientlen)) < 0){
-            printf("accept");
+        if ((newSock = accept(sock, clientptr, &clientlen)) < 0) {
             exit(EXIT_FAILURE);
         }
         globalData.bufferArray[globalData.counter++] = newSock;
         globalData.end = (globalData.end + 1) % bufferSize;
-        unlockMutex(globalData.mtx);
-        pthread_cond_broadcast(&globalData.condNotEmpty);
+        pthread_cond_broadcast(&condNotEmpty);
+        unlockMutex(mtx);
     }
 
     return 0;
